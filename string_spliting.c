567db74d3a61f12d857d02449c1179cff3677c50
h1t3re <lahbabi.chems.eddine@gmail.com>
2025-05-25 19:48:33 +0200

    Add files via upload

--- string_spliting.c
+++ string_spliting.c
@@ -4,8 +4,8 @@
 
 char **string_spliting(char *string, char *spliter)
 {
-	char **arrays = (char **)malloc(sizeof(char *));
-	char *temporary_array = (char *)malloc((strlen(spliter)+1)*sizeof(char));
+	char *temporary_array = (char *)malloc(strlen(spliter)*sizeof(char));
+	char **arrays = (char **)malloc(sizeof(char *));	
 	int position_in_arrays = 0;
 	int position_in_string = 0;
 	int position_of_string = 0;
@@ -27,36 +27,45 @@ char **string_spliting(char *string, char *spliter)
 		position_of_string++;	
 		temporary_array[j] = '\0';
 		arrays[position_in_arrays][position_of_string] = '\0';
-
-		if(!strcmp(strdup(temporary_array),strdup(spliter))) 
+		printf("%s\n", arrays[position_in_arrays]);
+		if(strcmp(strdup(temporary_array),strdup(spliter))) 
+		{
+			position_in_arrays++;
+			arrays = (char **)realloc(arrays, (position_in_arrays+1)*sizeof(char *));
+			arrays[position_in_arrays] = '\0';
+			position_of_string = 0;
+		}
+		if((j == strlen(spliter)) & (j == strlen(temporary_array)))
 		{
 			position_in_string = position_in_string+j;
 			j = 0;
-			position_of_string = 0;;
-			printf("temporary_array = %s\n", temporary_array);
-			printf("%s\n", arrays[position_in_arrays]);
 		}
 		//while(& (string[position_in_string+j] != '\0'));	
-		position_in_arrays++;
-
-		arrays = (char **)realloc(arrays, (position_in_arrays+1)*sizeof(char *));
-		arrays[position_in_arrays] = '\0';
+		
 	}
-	
+	arrays[position_in_arrays] = '\0';
+	free(temporary_array);
 	return arrays;
 }
 
 int main()
 {
-	char *string = strdup("00 11 22 33 44 55 66 77 88 99\0");
-	char *spliter = strdup("  \0");
+	char *string = strdup("0 1 2 3 4 5 6 7 8 9\0");
+	char *spliter = strdup(" \0");
 	int i = 0;
 	char **arrays = string_spliting(strdup(string), strdup(spliter));
-	/*while(*(arrays+(i*sizeof(char *))) != '\0')
+	while(arrays[i] != '\0')
 	{
-		printf("%s\n", *(arrays+(i*sizeof(char *))));
+		printf("%s", arrays[i]);
+		i++;	
+	}
+	free(string);
+	free(spliter);
+	while(*(arrays+(i*sizeof(char *))) != '\0')
+	{
+		free(arrays[i]);
 		i++;
-	}*/
+	}
+	free(arrays);
 	return 0;
 }
-
