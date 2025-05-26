e3d6ad78a43386e4696762fa40027fd82a9d11db
h1t3re <lahbabi.chems.eddine@gmail.com>
2025-05-26 15:03:56 +0200

    Add files via upload

--- string_spliting.c
+++ string_spliting.c
@@ -4,47 +4,47 @@
 
 char **string_spliting(char *string, char *spliter)
 {
-	char *temporary_array = (char *)malloc(strlen(spliter)*sizeof(char));
+	char *temporary_array = (char *)malloc(strlen(spliter)*sizeof(char));;
 	char **arrays = (char **)malloc(sizeof(char *));	
 	int position_in_arrays = 0;
+	arrays[position_in_arrays] = (char *)malloc(strlen(spliter)*sizeof(char));
 	int position_in_string = 0;
 	int position_of_string = 0;
-	int string_length = 0;
 	int j = 0;
 	int i = 0;
 	while(*(string+position_in_string+j) != '\0')
 	{
-		if(position_in_string == 0)
-			arrays[position_in_arrays] = (char *)malloc((position_in_string+1)*sizeof(char));
-		else
-			arrays[position_in_arrays] = (char *)realloc(arrays[position_in_arrays], (position_of_string+1)*sizeof(char));
-
-		temporary_array[j] = string[position_in_string+j];
-
-		arrays[position_in_arrays][position_of_string] = string[position_in_string+j];	
-		j++;
-		//position_in_string++;
-		position_of_string++;	
-		temporary_array[j] = '\0';
-		arrays[position_in_arrays][position_of_string] = '\0';
-		printf("%s\n", arrays[position_in_arrays]);
-		if(strcmp(strdup(temporary_array),strdup(spliter))) 
+		
+		while(j < strlen(spliter))
 		{
+			temporary_array[j] = string[position_in_string+j];
+			j++;
+		}
+		temporary_array[j] = '\0';
+		position_in_string = position_in_string +1;
+		printf("tmp = \'%s\'\n", temporary_array);
+		j = 0;
+		printf("comparaison(\'%s\', \'%s\') = %d\n", strdup(temporary_array), strdup(spliter), strcmp(strdup(temporary_array), strdup(spliter)));
+		if(strcmp(strdup(temporary_array), strdup(spliter)) != 0)
+		{
+			printf("in if\n");
+			++i;
+			arrays[position_in_arrays] = strconcatenate(strdup(arrays[position_in_arrays]), strdup(temporary_array));
+			position_of_string = position_of_string + strlen(spliter);
+			arrays[position_in_arrays][position_of_string] = '\0';
+			printf("arrays[%d] = %s\n", position_in_arrays, arrays[position_in_arrays]);
+		}else
+		{			
+			printf("in else\n");
+			arrays[position_in_arrays][position_of_string] = '\0';
+			free(temporary_array);
+			temporary_array = (char *)malloc(strlen(spliter)*sizeof(char));
 			position_in_arrays++;
 			arrays = (char **)realloc(arrays, (position_in_arrays+1)*sizeof(char *));
-			arrays[position_in_arrays] = '\0';
-			position_of_string = 0;
+			arrays[position_in_arrays] = (char *)malloc(strlen(spliter)*sizeof(char));
 		}
-		if((j == strlen(spliter)) & (j == strlen(temporary_array)))
-		{
-			position_in_string = position_in_string+j;
-			j = 0;
-		}
-		//while(& (string[position_in_string+j] != '\0'));	
-		
 	}
 	arrays[position_in_arrays] = '\0';
-	free(temporary_array);
 	return arrays;
 }
 
@@ -56,16 +56,17 @@ int main()
 	char **arrays = string_spliting(strdup(string), strdup(spliter));
 	while(arrays[i] != '\0')
 	{
-		printf("%s", arrays[i]);
+		printf("%d = %s\n", i, arrays[i]);
 		i++;	
 	}
-	free(string);
-	free(spliter);
-	while(*(arrays+(i*sizeof(char *))) != '\0')
+	
+	while(arrays[i] != '\0')
 	{
 		free(arrays[i]);
 		i++;
 	}
+	free(string);
+	free(spliter);
 	free(arrays);
 	return 0;
 }
